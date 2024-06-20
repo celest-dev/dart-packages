36c6c07cc1b5444a4cf442e607dd8c5b2604c268
Dillon Nys <dillon.andre.nys@gmail.com>
2024-06-20 11:53:46 -0700

    fix(linux): Null pointer on clear

--- packages/native/storage/lib/src/util/native.dart
+++ packages/native/storage/lib/src/util/native.dart
@@ -7,3 +7,16 @@ DynamicLibrary? tryOpenDylib(String name) {
     return null;
   }
 }
+
+DynamicLibrary searchDylib(String name, List<String> paths) {
+  for (final path in paths) {
+    final dylib = tryOpenDylib(path);
+    if (dylib != null) {
+      return dylib;
+    }
+  }
+  throw Exception(
+    'Could not find dylib for "$name". Tried paths:\n'
+    '${paths.map((it) => '- $it').join('\n')}',
+  );
+}
